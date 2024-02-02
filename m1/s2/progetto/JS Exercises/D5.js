9e6e3c56511d2f985988eca033d129c04ad84bcc
saraporri <saraporricino@yahoo.com>
2024-02-02 15:37:52 +0100

    esercizio 9

--- m1/s2/progetto/JS Exercises/D5.js
+++ m1/s2/progetto/JS Exercises/D5.js
@@ -103,6 +103,11 @@ const numericArray = [
   6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105,
 ];
 
+let i = 0;
+while (numericArray[i] <= 32) {
+  console.log(numericArray[i]);
+  i++;
+}
 /* ESERCIZIO 10
     Partendo dall'array fornito e utilizzando un costrutto switch, genera un nuovo array composto dalle posizioni di ogni carattere all'interno
     dell'alfabeto italiano.
