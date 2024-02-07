e66b712d9dd8e0b0577f6c5b9ec6ac4d7f72d5b5
saraporri <saraporricino@yahoo.com>
2024-02-07 16:42:56 +0100

    fixed code

--- m1/s3/g3/js/index.js
+++ m1/s3/g3/js/index.js
@@ -5,12 +5,15 @@ invia.addEventListener("click", function (event) {
 
   let listElement = document.createElement("p");
 
-  listElement.textContent = input.value;
+  listElement.innerText = input.value;
 
   let lista = document.querySelector("#lista");
   lista.append(listElement);
-  listElement.addEventListener("click", function (event) {
-    event.preventDefault();
-    listElement.style.textDecoration = "line-through";
+  listElement.addEventListener("click", function () {
+    if (listElement.style.textDecoration === "line-through") {
+      listElement.style.textDecoration = "none";
+    } else {
+      listElement.style.textDecoration = "line-through";
+    }
   });
 });
