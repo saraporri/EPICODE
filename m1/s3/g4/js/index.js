794006ea9babee172f193c6393e52b98eb6b8791
saraporri <saraporricino@yahoo.com>
2024-02-09 11:25:29 +0100

    progetto initial commit

--- m1/s3/g4/js/index.js
+++ m1/s3/g4/js/index.js
@@ -5,11 +5,16 @@ for (let i = 1; i <= 90; i++) {
   numero.innerText = i;
   numWrap.append(numero);
 }
+let numero = document.querySelector(".numero");
 
 const pescaBtn = document.querySelector(".pesca");
 let numPescato = document.querySelector(".numero-pescato");
 pescaBtn.addEventListener("click", function () {
   let numRandom = Math.floor(Math.random() * 91);
   numPescato.innerText = numRandom;
+  if (numPescato.innerText == numero.innerText) {
+    numero.classList.add("random");
+  }
 });
+
 console.log(numPescato, pescaBtn);
