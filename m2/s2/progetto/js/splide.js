c8aae0308e6cf10bd5228e14b39ea1e3ab34e0ee
saraporri <saraporricino@yahoo.com>
2024-03-01 15:05:21 +0100

    layout completed

--- m2/s2/progetto/js/splide.js
+++ m2/s2/progetto/js/splide.js
@@ -1,6 +1,47 @@
-var splide = new Splide(".splide", {
+var splide = new Splide("#image-carousel", {
   perPage: 6,
   gap: "2rem",
+  autoWidth: true,
+
+  breakpoints: {
+    640: {
+      perPage: 2,
+      gap: ".7rem",
+      height: "6rem",
+    },
+    480: {
+      perPage: 1,
+      gap: ".7rem",
+      height: "6rem",
+    },
+  },
+});
+
+splide.mount();
+var splide = new Splide("#image-carousel2", {
+  perPage: 6,
+  gap: "2rem",
+  autoWidth: true,
+
+  breakpoints: {
+    640: {
+      perPage: 2,
+      gap: ".7rem",
+      height: "6rem",
+    },
+    480: {
+      perPage: 1,
+      gap: ".7rem",
+      height: "6rem",
+    },
+  },
+});
+
+splide.mount();
+var splide = new Splide("#image-carousel3", {
+  perPage: 6,
+  gap: "2rem",
+  autoWidth: true,
   breakpoints: {
     640: {
       perPage: 2,
