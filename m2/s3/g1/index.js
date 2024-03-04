f91a0c21e9c7f3f67cc1db3e321e68118a67b092
saraporri <saraporricino@yahoo.com>
2024-03-04 14:59:52 +0100

    esercizio 1

---
+++ m2/s3/g1/index.js
@@ -0,0 +1,29 @@
+let pippi = {
+  firstName: "Pippi",
+  lastName: "Poppi",
+  age: 22,
+  location: "London",
+};
+
+class User {
+  constructor(firstName, age) {
+    this.firstName = firstName;
+    this.lastName = "Rossi";
+    this.age = age;
+    this.location = "Rome";
+  }
+
+  compareAges() {
+    if (pippi.age == this.age) {
+      return `${pippi.firstName} e ${this.firstName} sono coetanee`;
+    } else if (pippi.age > this.age) {
+      return `${pippi.firstName} è più piccola di ${this.firstName}`;
+    } else {
+      return `${this.firstName} è più grande di ${pippi.firstName}`;
+    }
+  }
+}
+
+let sara = new User("Sara", 29);
+let result = sara.compareAges();
+console.log(result);
