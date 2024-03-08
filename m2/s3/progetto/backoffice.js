8a67a6a32e882da2e76f30823d09721b5628b168
saraporri <saraporricino@yahoo.com>
2024-03-08 14:41:53 +0100

    si vede qualcosa

--- m2/s3/progetto/backoffice.js
+++ m2/s3/progetto/backoffice.js
@@ -1,87 +1,35 @@
-const addressBarContent = new URLSearchParams(location.search);
-const eventId = addressBarContent.get("eventId");
+const submit = document.getElementById("submit");
 
-if (eventId) {
-  fetch("https://striveschool-api.herokuapp.com/api/product/" + eventId, {
-    headers: {
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWVhZTM4ODJkN2IxMTAwMTkwZTZlYjAiLCJpYXQiOjE3MDk4OTI0ODgsImV4cCI6MTcxMTEwMjA4OH0.8-vfD3NUF5uCqMbozqiGS5ORSyhdC-XDtMAUgQlCIHY",
-    },
-  })
-    .then((res) => {
-      if (res.ok) {
-        return res.json();
-      } else {
-        throw new Error("ERRORE NEL RECUPERO DETTAGLIO");
-      }
-    })
-    .then((cell) => {
-      const nameInput = document.getElementById("name");
-      const descriptionInput = document.getElementById("description");
-      const priceInput = document.getElementById("price");
-      const imageUrlInput = document.getElementById("imageUrl");
-      const brandInput = document.getElementById("brand");
-
-      nameInput.value = cell.name;
-      descriptionInput.value = cell.description;
-      priceInput.value = cell.price;
-      brandInput.value = cell.brand;
-      imageUrlInput.value = cell.imageUrl;
-    })
-    .catch((err) => {
-      console.log("err", err);
-    });
-}
-
-const formRif = document.getElementById("form");
-formRif.addEventListener("submit", function (e) {
+submit.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log("invio dati");
-  const nameInput = document.getElementById("name");
-  const descriptionInput = document.getElementById("description");
-  const priceInput = document.getElementById("price");
-  const imageUrlInput = document.getElementById("imageUrl");
-  const brandInput = document.getElementById("brand");
+  console.log("Invio dati");
+
+  const name = document.getElementById("name").value;
+  const description = document.getElementById("description").value;
+  const price = document.getElementById("price").value;
+  const imageUrl = document.getElementById("imageUrl").value;
+  const brand = document.getElementById("brand").value;
 
   const newProduct = {
-    name: nameInput.value,
-    description: descriptionInput.value,
-    price: priceInput.value,
-    brand: brandInput.value,
-    imageUrl: imageUrlInput.value,
+    name,
+    description,
+    price,
+    imageUrl,
+    brand,
   };
-  console.log("Ecco l'oggetto che manderò alle API", newProduct);
 
-  let methodToUse = "POST";
-  if (eventId) {
-    methodToUse = "PUT";
-  }
-
-  let urlToUse = "https://striveschool-api.herokuapp.com/api/product";
-  if (eventId) {
-    urlToUse = "https://striveschool-api.herokuapp.com/api/product/" + eventId;
-  }
-
-  fetch(urlToUse, {
-    method: methodToUse,
-    body: JSON.stringify(newProduct),
+  fetch("https://striveschool-api.herokuapp.com/api/product/", {
+    method: "POST",
     headers: {
+      "Content-type": "application/json",
+
       Authorization:
         "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWVhZTM4ODJkN2IxMTAwMTkwZTZlYjAiLCJpYXQiOjE3MDk4OTI0ODgsImV4cCI6MTcxMTEwMjA4OH0.8-vfD3NUF5uCqMbozqiGS5ORSyhdC-XDtMAUgQlCIHY",
-      "Content-Type": "application/json",
     },
+    body: JSON.stringify(newProduct),
   })
+    .then((res) => res.json())
     .then((res) => {
-      console.log("ogg", res);
-      if (res.ok) {
-        alert("Prodotto salvato");
-        location.href = "index.html";
-      } else {
-        alert("Qualcosa è andato storto");
-        throw new Error("err in fase di POST");
-      }
-    })
-    .catch((err) => {
-      console.log("Errore ", err);
+      location.href = "index.html";
     });
 });
---
+++ m2/s3/progetto/index.js
@@ -0,0 +1,32 @@
+fetch("https://striveschool-api.herokuapp.com/api/product/", {
+  method: "GET",
+  headers: {
+    "Content-type": "application/json",
+
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWVhZTM4ODJkN2IxMTAwMTkwZTZlYjAiLCJpYXQiOjE3MDk4OTI0ODgsImV4cCI6MTcxMTEwMjA4OH0.8-vfD3NUF5uCqMbozqiGS5ORSyhdC-XDtMAUgQlCIHY",
+  },
+})
+  .then((res) => res.json())
+  .then((newProducts) => {
+    console.log(newProducts);
+    for (let newProduct of newProducts) {
+      let card = createClone();
+
+      let img = card.querySelector("#img");
+      let name = card.querySelector("#name");
+      let price = card.querySelector("#price");
+
+      img.src = newProduct.imageUrl;
+      name.innerText = newProduct.name;
+      price.innerText = newProduct.price;
+
+      document.querySelector(".row").append(card);
+    }
+  });
+
+function createClone() {
+  let template = document.querySelector("#card-template");
+  let clone = template.content.cloneNode(true);
+  return clone;
+}
