8032ac687d58b9a50bb2e83053af009b42170fb3
saraporri <saraporricino@yahoo.com>
2024-03-18 17:58:48 +0100

    risolto

--- m3/s1/g1/esercizio.js
+++ m3/s1/g1/esercizio.js
@@ -1,1 +1,31 @@
 "use strict";
+class User {
+    constructor(nome, cognome, credito) {
+        this.credito = 0;
+        this.numeroChiamate = 0;
+        this.costoMinuto = 0.2;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.credito = credito || 0;
+    }
+    ricarica(credito) {
+        this.credito += credito;
+    }
+    chiamata(minuti) {
+        let costo = this.costoMinuto * minuti;
+        this.credito -= costo;
+        this.numeroChiamate++;
+    }
+    chiama404() {
+        return this.credito + "euro";
+    }
+    getNumberoChiamate() {
+        return this.numeroChiamate;
+    }
+    azzeraChiamate() {
+        this.numeroChiamate = 0;
+    }
+}
+let user = new User("Sara", "Porricino");
+user.ricarica(50);
+user.chiamata(20);
--- m3/s1/g1/esercizio.ts
+++ m3/s1/g1/esercizio.ts
@@ -1,4 +1,36 @@
-interface Smartphone {
+interface iSmartphone {
   credito: number;
   numeroChiamate: number;
 }
+class User implements iSmartphone {
+  credito: number = 0;
+  numeroChiamate: number = 0;
+  costoMinuto: number = 0.2;
+  nome: string;
+  cognome: string;
+  constructor(nome: string, cognome: string, credito?: number) {
+    this.nome = nome;
+    this.cognome = cognome;
+    this.credito = credito || 0;
+  }
+  ricarica(credito: number): void {
+    this.credito += credito;
+  }
+  chiamata(minuti: number): void {
+    let costo = this.costoMinuto * minuti;
+    this.credito -= costo;
+    this.numeroChiamate++;
+  }
+  chiama404(): string {
+    return this.credito + "euro";
+  }
+  getNumberoChiamate(): number {
+    return this.numeroChiamate;
+  }
+  azzeraChiamate(): void {
+    this.numeroChiamate = 0;
+  }
+}
+let user = new User("Sara", "Porricino");
+user.ricarica(50);
+user.chiamata(20);
