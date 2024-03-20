7ab5a8a8c0693b3f7a09dca8bfed7e8c0f3eeb76
saraporri <saraporricino@yahoo.com>
2024-03-20 16:06:46 +0100

    nada

--- m3/s1/g3/esercizioD3/src/app/app.component.ts
+++ m3/s1/g3/esercizioD3/src/app/app.component.ts
@@ -8,3 +8,30 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'esercizioD3';
 }
+
+export interface iArticolo {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+  tags: string[];
+  active: boolean;
+}
+export class AppComponent {
+
+  articoli:iArticolo[] = [];
+
+  ngOnInit(){
+
+    fetch('../assets/pizze.json')
+    .then(pizze => pizze.json())
+    .then((pizze:iPizze[]) => this.pizzeArr = pizze)
+
+  }
+
+  soloDisponibili(){
+    this.pizzeArr = this.pizzeArr.filter(p => p.disponibile)
+  }
+
+}
+
