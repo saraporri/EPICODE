19c4ddba53631185935aa1c22f5e1e55fc2917b1
saraporri <saraporricino@yahoo.com>
2024-03-22 11:49:29 +0100

    routes created

--- m3/s1/progetto/autosalone/src/app/app-routing.module.ts
+++ m3/s1/progetto/autosalone/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AudiComponent } from './components/audi/audi.component';
+import { FiatComponent } from './components/fiat/fiat.component';
+import { FordComponent } from './components/ford/ford.component';
+const routes: Routes = [
 
-const routes: Routes = [];
+{path:"",component:HomeComponent},{path:"audi",component:AudiComponent},{path:"fiat",component:FiatComponent},{path:"ford",component:FordComponent}
+
+
+
+
+
+
+
+
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
