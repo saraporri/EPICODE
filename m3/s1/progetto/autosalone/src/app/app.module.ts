ce364067e0ad51fcf2378676128b0196114b13cc
saraporri <saraporricino@yahoo.com>
2024-03-22 10:43:08 +0100

    created main components

--- m3/s1/progetto/autosalone/src/app/app.module.ts
+++ m3/s1/progetto/autosalone/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './main-components/footer/footer.component';
+import { NavBarComponent } from './main-components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
