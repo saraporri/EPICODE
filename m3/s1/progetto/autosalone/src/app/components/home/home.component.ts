55afd8e89be574cebb03f69d71bd5b36f46d7280
saraporri <saraporricino@yahoo.com>
2024-03-22 13:15:30 +0100

    random cars

--- m3/s1/progetto/autosalone/src/app/components/home/home.component.ts
+++ m3/s1/progetto/autosalone/src/app/components/home/home.component.ts
@@ -13,7 +13,7 @@ import { Component } from '@angular/core';
 
 export class HomeComponent {
     cars!: iCars[];
-    randomCar: iCars[] = [];
+    randomCars: iCars[] = [];
     brands!: string[];
 
 constructor(private carsSvc:CarsService){}
@@ -21,6 +21,19 @@ constructor(private carsSvc:CarsService){}
 
 
     ngOnInit(): void {
-        this.carsSvc.getAllCars().then(res=>{this.cars=res});
+        this.carsSvc.getAllCars().then(res=>{this.cars=res;this.getRandomCars(2)});
+    }
+
+
+
+    getRandomCars(n: number): void {
+      const randomIndices: number[] = [];
+      while (randomIndices.length < n) {
+        const randomIndex = Math.floor(Math.random() * this.cars.length);
+        if (!randomIndices.includes(randomIndex)) {
+          randomIndices.push(randomIndex);
+          this.randomCars.push(this.cars[randomIndex]);
+        }
+      }
     }
   }
