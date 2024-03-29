1c68e29c3a13cd58e67053321f222a03df5673ae
saraporri <saraporricino@yahoo.com>
2024-03-29 16:40:48 +0100

    completed created

--- m3/s2/progetto/TodoSara/src/app/components/completed/completed.component.ts
+++ m3/s2/progetto/TodoSara/src/app/components/completed/completed.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import { TodoService } from '../../services/todo.service';
+import { iTodo } from '../../models/todo';
 @Component({
   selector: 'app-completed',
   templateUrl: './completed.component.html',
   styleUrl: './completed.component.scss'
 })
 export class CompletedComponent {
+completedTodos:iTodo[]=[]
+constructor(private todoSvc: TodoService) {}
+
+ngOnInit() {
+  this.completedTodos = this.todoSvc.getCompletedTodos();
+}
 
 }
