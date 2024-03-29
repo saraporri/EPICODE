28a44a7adc368f4b7704e238c24fe837a88b7d28
saraporri <saraporricino@yahoo.com>
2024-03-29 11:38:28 +0100

    components and routes created

--- m3/s2/progetto/TodoSara/src/app/app-routing.module.ts
+++ m3/s2/progetto/TodoSara/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CompletedComponent } from './components/completed/completed.component';
+import { UsersComponent } from './components/users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:"/",component:HomeComponent},
+{path:"/completed",component:CompletedComponent},
+{path:"/users",component:UsersComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- m3/s2/progetto/TodoSara/src/app/app.module.ts
+++ m3/s2/progetto/TodoSara/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './main-components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { CompletedComponent } from './components/completed/completed.component';
+import { UsersComponent } from './components/users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    CompletedComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
